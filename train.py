5210172628381e3a067b5cc06e424c41d4350896
khai9xht <khaihoang9x@gmail.com>
2021-04-13 09:27:26 +0700

    update

--- train.py
+++ train.py
@@ -39,7 +39,7 @@ def train_pose(pose_net, imgs, labels, criterion, optim):
     return loss
 
 
-def train(gen, dis, pose_net, dataloaders,optims, criterions, schedulers, epochs):
+def train_st1(gen, dis, pose_net, dataloaders,optims, criterions, schedulers, epochs):
     g_optim, d_optim, h_optim = optims
     gan_loss, hp_loss = criterions
     g_ched, d_sched, h_sched = schedulers
@@ -52,6 +52,7 @@ def train(gen, dis, pose_net, dataloaders,optims, criterions, schedulers, epochs
             param.requires_grad = False
         for param in dis.parameters():
             param.requires_grad = True
+
         GAN_total_loss = 0
         for iteration, batch in enumerate(Rdataloader):
             real_imgs, real_labels = batch
@@ -63,21 +64,26 @@ def train(gen, dis, pose_net, dataloaders,optims, criterions, schedulers, epochs
             fake_imgs = gen(fake_vectors)
             loss = train_d(dis, gan_loss, d_optim, fake_imgs, fake_labels)
             GAN_total_loss += loss
+
         GAN_total_loss.forward()
         d_sched.step()
+
 
         # Train generator model
         for param in gen.parameters():
             param.requires_grad = True
         for param in dis.parameters():
             param.requires_grad = False
+
         GAN_total_loss = 0
         for iteration, batch in enumerate(Fdataloader):
             fake_vectors, fake_labels = batch
             fake_imgs = gen(fake_vectors)
             loss = train_d(dis, gan_loss, d_optim, fake_imgs, fake_labels)
             GAN_total_loss += loss
+
         GAN_total_loss.backward()
+
 
         # Train head pose model
         HP_total_loss = 0
@@ -85,6 +91,70 @@ def train(gen, dis, pose_net, dataloaders,optims, criterions, schedulers, epochs
             real_imgs, real_labels = batch
             loss = train_pose(pose_net, real_imgs, real_labels, hp_loss, h_optim)
             HP_total_loss += loss
+
+        HP_total_loss.backward()
+        h_sched.step()
+
+def train_st2(gen, dis, pose_net, dataloaders,optims, criterions, schedulers, epochs):
+    g_optim, d_optim, h_optim = optims
+    gan_loss, hp_loss = criterions
+    g_ched, d_sched, h_sched = schedulers
+    Fdataloader, Rdataloader, HPdataloader = dataloaders
+
+    for epoch in tqdm(range(epochs)):
+
+        # Train discriminator model
+        for param in gen.parameters():
+            param.requires_grad = False
+        for param in dis.parameters():
+            param.requires_grad = True
+
+        total_loss = 0
+        for iteration, batch in enumerate(Rdataloader):
+            real_imgs, real_labels = batch
+            loss = train_d(dis, gan_loss, d_optim, real_imgs, real_labels)
+            total_loss += loss
+
+        for iteration, batch in enumerate(Fdataloader):
+            fake_vectors, fake_labels = batch
+            fake_imgs = gen(fake_vectors)
+            loss = train_d(dis, gan_loss, d_optim, fake_imgs, fake_labels)
+            total_loss += loss
+
+        total_loss.forward()
+        d_sched.step()
+
+
+        # Train generator model
+        for param in gen.parameters():
+            param.requires_grad = True
+        for param in dis.parameters():
+            param.requires_grad = False
+        for param in pose_net.parameters():
+            param.requires_grad = False
+
+        total_loss = 0
+        for iteration, batch in enumerate(Fdataloader):
+            fake_vectors, fake_labels = batch
+            fake_imgs = gen(fake_vectors)
+            loss = train_d(dis, gan_loss, d_optim, fake_imgs, fake_labels)
+            total_loss += loss
+            pred_pose = pose_net(fake_imgs)
+            total_loss += hp_loss(pred_pose, fake_vectors)
+
+        total_loss.backward()
+
+
+        # Train Head pose model
+        for param in pose_net.parameters():
+            param.requires_grad = True
+
+        HP_total_loss = 0
+        for iteration, batch in enumerate(HPdataloader):
+            real_imgs, real_labels = batch
+            loss = train_pose(pose_net, real_imgs, real_labels, hp_loss, h_optim)
+            HP_total_loss += loss
+
         HP_total_loss.backward()
         h_sched.step()
 
